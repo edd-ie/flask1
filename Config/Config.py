e28c187c02911e6daf7b567e3cbd002feac6af59
edd-ie <ogendoeddie@gmail.com>
2019-12-19 21:19:56 +0300

    2nd

--- Config/Config.py
+++ Config/Config.py
@@ -3,3 +3,8 @@ class Development():
     SECRET_KEY = '80445a717e76a537ef9722986ac2a074'
     DEBUG = True
 
+
+class Production():
+    SQLALCHEMY_DATABASE_URI = 'postgres://rycipishxbaqec:9f93edfd69965d870ff65bcf0170ed1f0c3c985b426c3db9683d9b2ca2ffbf9a@ec2-54-247-177-254.eu-west-1.compute.amazonaws.com:5432/d4ojr2pqhiarvt'
+    SECRET_KEY = '80445a717e76a537ef9722986ac2a074'
+    DEBUG = False
