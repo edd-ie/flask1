919574af1afcafc6f29746ff208fcadf9f69828a
edd-ie <ogendoeddie@gmail.com>
2019-12-23 18:32:33 +0300

    finale

--- Config/Config.py
+++ Config/Config.py
@@ -1,5 +1,5 @@
 class Development():
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:sigma-omega@127.0.0.1:5432/sales_demo'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:secret@127.0.0.1:5432/sales_demo'
     SECRET_KEY = '80445a717e76a537ef9722986ac2a074'
     DEBUG = True
 
